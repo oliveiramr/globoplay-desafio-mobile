44e5fa07fe02187c35b86e6393fae17d914a497c
Murilo R. Oliveira <Murilo.RibeiroDeOliveira-ext@ab-Inbev.com>
2024-12-17 22:46:25 -0300

    Create initial coordinator and add assets

---
+++ globoplay/Globoplay/Supporting Files/GloboplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloboplayApp.swift
+//  Globoplay
+//
+//  Created by Murilo on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GloboplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppCoordinator().start()
+        }
+    }
+}
